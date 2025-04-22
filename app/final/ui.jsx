370ee1e88d6405407543680837c4059b376dd62d
young <hi-youngss>
2025-04-22 16:01:43 +0900

    0422 배포코드

--- app/final/ui.jsx
+++ app/final/ui.jsx
@@ -38,6 +38,7 @@ const ImageRectangle = ({
   const [image] = useImage(src, "anonymous");
 
   const shapeRef = useRef();
+  const trRef = useRef();
 
   const [style, api] = useSpring(() => ({
     width: shapeProps.width,
@@ -111,6 +112,17 @@ const ImageRectangle = ({
     }
   }, [undoRedoState]);
 
+  useEffect(() => {
+    if (isSelected) {
+      // we need to attach transformer manually
+      trRef.current.nodes([shapeRef.current]);
+    }
+    if (isSelected && trRef.current && shapeRef.current) {
+      trRef.current.nodes([shapeRef.current]);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
+
   return (
     <>
       <animated.Image
@@ -119,8 +131,51 @@ const ImageRectangle = ({
         ref={shapeRef}
         onClick={onSelect}
         onTap={onSelect}
-        {...style}
+        // {...style}
+        {...shapeProps}
+        onDragEnd={(e) => {
+          onChange({
+            ...shapeProps,
+            x: e.target.x(),
+            y: e.target.y(),
+          });
+        }}
+        onTransformEnd={(e) => {
+          // transformer is changing scale of the node
+          // and NOT its width or height
+          // but in the store we have only width and height
+          // to match the data better we will reset scale on transform end
+          const node = shapeRef.current;
+          const scaleX = node.scaleX();
+          const scaleY = node.scaleY();
+
+          // we will reset it back
+          node.scaleX(1);
+          node.scaleY(1);
+          onChange({
+            ...shapeProps,
+            x: node.x(),
+            y: node.y(),
+            // set minimal value
+            width: Math.max(5, node.width() * scaleX),
+            height: Math.max(node.height() * scaleY),
+            rotation: node.rotation(),
+          });
+        }}
       />
+      {isSelected && (
+        <Transformer
+          ref={trRef}
+          flipEnabled={false}
+          boundBoxFunc={(oldBox, newBox) => {
+            // limit resize
+            if (Math.abs(newBox.width) < 5 || Math.abs(newBox.height) < 5) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+        />
+      )}
     </>
   );
 };
